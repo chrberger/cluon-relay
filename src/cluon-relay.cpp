33f9f489f90919003014b5a775d28aa65f5751e5
olbender <olbender@users.noreply.github.com>
2020-09-29 00:25:12 +0200

    Added many-to-one pattern

--- src/cluon-relay.cpp
+++ src/cluon-relay.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019  Christian Berger, Ola Benderius
+ * Copyright (C) 2020  Christian Berger
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
